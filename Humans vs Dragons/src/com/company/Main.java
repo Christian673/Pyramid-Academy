f169aa4304d7891bd7aa59d23c2a3b85b4c6dc69
Christian673 <67290704+Christian673@users.noreply.github.com>
2022-03-08 12:28:52 -0500

    Add files via upload

---
+++ Humans vs Dragons/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int cave = 0;
+        System.out.println("enter cave 1 or cave 2");
+
+        if(cave > 1 ){
+            System.out.println("I am a good Dragon and I will share my gold");
+        }
+        else if (cave < 2){
+            System.out.println("A Dragon swoops down and gobbles you up");
+        }
+    }
+}
